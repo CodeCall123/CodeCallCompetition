204ad0083e94ef54ac2eb3cd7cb8a6c7790195fa
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2024-10-17 01:48:26 +0530

    add verifyAuth to routes

--- mybackend/routes/competitions.js
+++ mybackend/routes/competitions.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const checkRole = require('../middleware/checkRole');
 const CompetitionController  = require('../controller/competition');
+const verifyAuth = require('../middleware/verifyAuth');
 
 const router = express.Router();
 
@@ -8,10 +9,10 @@ const competitionController = new CompetitionController();
 
 router.get('/competitions', competitionController.allCompetitions);
 router.get('/competitions/:id', competitionController.selectedCompetition);
-router.post("/competitions/:id/addJudge", competitionController.addJudge);
-router.patch('/competitions/:id/becomeJudge', competitionController.makeJudge);
-router.post('competitions/:id/mergePR', checkRole('judge'), competitionController.approveAndMergePR);
-router.put('/competitions/:id/changeStatus', competitionController.updateCompetitionStatus);
-router.put('/competitions/:id/approveSubmission', competitionController.approveSubmission);
+router.post("/competitions/:id/addJudge", verifyAuth, competitionController.addJudge);
+router.patch('/competitions/:id/becomeJudge', verifyAuth, competitionController.makeJudge);
+router.post('competitions/:id/mergePR', verifyAuth, checkRole('judge'), competitionController.approveAndMergePR);
+router.put('/competitions/:id/changeStatus', verifyAuth, competitionController.updateCompetitionStatus);
+router.put('/competitions/:id/approveSubmission', verifyAuth, competitionController.approveSubmission);
 
 module.exports = router;
--- mybackend/routes/training.js
+++ mybackend/routes/training.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const TrainingController = require('../controller/training');
+const verifyAuth = require('../middleware/verifyAuth');
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ const trainingModuleController = new TrainingController();
 
 router.get('/training', trainingModuleController.trainingModules);
 router.get('/training/:id', trainingModuleController.fetchTrainingModule);
-router.post('/training/:id/submit', trainingModuleController.submitTrainingModuleWork);
+router.post('/training/:id/submit', verifyAuth, trainingModuleController.submitTrainingModuleWork);
 
 module.exports = router;
